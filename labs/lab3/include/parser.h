64430147989140ff35b8bea36abac8cd4a53a31b
sanueee <skarin555@gmail.com>
2026-03-31 19:43:25 +0300

    parser.c done

--- labs/lab3/include/parser.h
+++ labs/lab3/include/parser.h
@@ -3,6 +3,32 @@
 
 #include "../include/ast.h"
 
+typedef enum
+{
+    TOK_EOF,
+    TOK_NUM,
+    TOK_VAR,
+    TOK_OP,
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_ERROR
+} TokType;
+
+typedef struct
+{
+    TokType type;
+    int num;
+    char var[64];
+    char op;
+} Token;
+
+typedef struct
+{
+    const char *src;
+    int pos;
+    int error;
+} Lexer;
+
 ASTNode *parse_expr(const char *expr);
 ASTNode *parse_prefix(const char *expr);
 ASTNode *parse_postfix(const char *expr);
